ce7171af00c693c8a6bb25a4bf2832a76a331bfd
Salvador <sfandino@yahoo.com>
2012-07-12 15:20:55 +0200

    working on templates

--- lib/Module/CAPIMaker/Template/Module_H.pm
+++ lib/Module/CAPIMaker/Template/Module_H.pm
@@ -19,10 +19,13 @@ __DATA__
 #define <% $module_h_barrier %>
 
 #define <% uc $c_module_name %>_C_API_REQUIRED_VERSION <% $max_version %>
-
-void perl_<% $c_module_name %>_load(int required_version);
+<% $module_h_beginning %>
+int perl_<% $c_module_name %>_load(int required_version);
 
 #define PERL_<% uc $c_module_name %>_LOAD perl_<% $c_module_name %>_load(<% uc $c_module_name %>_C_API_REQUIRED_VERSION)
+#define PERL_<% uc $c_module_name %>_LOAD_OR_CROAK \
+    if (PERL_<% uc $c_module_name %>_LOAD);        \
+    else croak(NULL);
 
 extern HV *<% $c_module_name %>_c_api_hash;
 extern int <% $c_module_name %>_c_api_min_version;
@@ -32,9 +35,14 @@ extern int <% $c_module_name %>_c_api_max_version;
     for my $n (sort keys %function) {
         my $f = $function{$n};
         my $var = "${c_module_name}_c_api_$n";
-        $OUT .= "extern $f->{type} *($var)($f->{args});\n";
-        $OUT .= "#define $export_prefix$n (*$var)\n";
+        $OUT .= "extern $f->{type} (*$var)($f->{args});\n";
+        if ($f->{pTHX}) {
+            $OUT .= "#define $export_prefix$n($f->{macro_args}) ((*$var)($f->{call_args}))\n";
+        }
+        else {
+            $OUT .= "#define $export_prefix$n (*$var)\n";
+        }
     }
 %>
-
+<% $module_h_end %>
 #endif
